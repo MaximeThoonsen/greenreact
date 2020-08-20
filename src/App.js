9bbb2cbe833f463f3179fc85f724c5daeba25d7c
Maxime Thoonsen <maximet@theodo.fr>
2020-08-21 00:38:38 +0200

    add images and dynamic content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import plane from './plane.jpg';
+import plane2 from './plane2.jpg';
+import plane3 from './plane3.jpg';
 
-function displayGreen() {
+function displayGreenImage(imageSrc, altText) {
 
   if (navigator.connection.type === 'cellular'){
-    return <p>logo</p>
+    return <div className='photoText'>{altText}</div>
   }
+
 
   console.log(navigator)
   console.log(navigator.connection)
   console.log(navigator.connection.type)
 
-  return <img src={logo} className="App-logo" alt="logo" />
+  return <img src={imageSrc} className="App-logo" alt="logo" />
 }
+
 
 
 function App() {
@@ -21,19 +26,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         {
-          displayGreen()
+          displayGreenImage(plane, 'Nice photo of a plane')
         }
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Travel is fun.Blablabla
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          displayGreenImage(plane2, 'One more photo of a nice plane')
+        }
+
+        <p>
+          This mojito was amazing. We found it in a bar..... blabla
+        </p>
+
+        {
+          displayGreenImage(plane3, 'Last photo of a nice plane')
+        }
+
+        <p>
+          A great story and by luck this website is optimized for cellular content.
+          On the cellular network this page weigth 861kB because we remove the images and with the WIFI this page weight 2.5 MB
+        </p>
+
       </header>
     </div>
   );
